ef3687b7260ce3fae91f6538e54837a983a527da
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-10-23 02:22:22 +0000

    First config for webpack. Just for testing. Should be divided to developers, testing and production environments.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = [
+//  {
+//    entry: './index.js',
+//    output:  {
+//      path: __dirname,
+//      filename: 'server.js'
+//    },
+//    target: 'node',
+//    externals: nodeExternals()
+//  },
+  {
+    entry: './src/spapp/index.jsx',
+    output: {
+      path: __dirname + '/static',
+      filename: 'app.js'
+    },
+    module: {
+      rules: [
+        {
+          oneOf: [
+            {
+              test: /\.jsx$/,
+              loader: require.resolve('babel-loader'),
+              options: {
+                compact: true,
+                presets: ['react']
+              }
+            },
+            {
+              test: /\.css$/,
+              use: [
+                { loader: 'style-loader' },
+                { loader: 'css-loader' }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+]
