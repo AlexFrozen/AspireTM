dab3d2d6b989df95c65ef3847054403e754e6579
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-10-28 07:41:34 +0000

    Fixed Users' data structure.

---
+++ src/init.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb'
+import assert from 'assert'
+import crypto from 'crypto'
+
+const login = 'admin@no.mail'
+const pwd = 'admin'
+const pass = crypto.createHash('sha256').update(pwd).digest('hex')
+
+MongoClient.connect('mongodb://localhost:27017/aspire-tm', (err, db) => {
+  assert.equal(null, err);
+  console.log('Connected successfully to MongoDB server');
+
+  const col = db.collection('Users')
+  col.findOneAndUpdate({eMail:'admin@no.mail'}, {$set: {password:pass}}, (err, r) => {
+    if (r.value == null) {
+      console.log('Default user is not found, trying to create...')
+      col.insertOne({
+        firstName: 'Aspire',
+        lastName: 'Administrator',
+        manager: {
+          id: 0,
+          fullName: 'None'
+        },
+        eMail: login,
+        role: 'Admin',
+        password: pass
+      }, (err, r) => {
+        if (r.insertedCount != 1) {
+          console.log("Couldn't create default user. Sorry...")
+        }else{
+          console.log('Created default user ' + login + ' with pass ' + pwd + '. Don\'t forget to change!')
+        }
+      })
+    }else{
+      console.log('Password for default user ' + login + ' reset to ' + pwd)
+    }
+    db.close()
+  })
+})
