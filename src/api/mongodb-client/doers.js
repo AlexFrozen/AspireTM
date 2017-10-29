3891e69239cb2a9bac2a9cdf37d96ef4b007350e
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-10-29 10:06:49 +0000

    List doers accessible for User to post task.

---
+++ src/api/mongodb-client/doers.js
@@ -0,0 +1,44 @@
+function doers (req, res, db) {
+  const answer = {}
+
+  const Tokens = db.collection('Tokens')
+  Tokens.findOne(
+    {
+      token: req.params.token,
+    }, (err, r) => {
+      if (err) {
+        answer.status = 500
+        res.status(answer.status).json(answer)
+      }else if (r) {
+        const idManager = r.idUser
+        const Users = db.collection('Users')
+        const userlist = []
+
+        Users.find(
+          {
+            "manager.id": idManager
+          }).each((err, r) => {
+          if (err) {
+            answer.status = 500
+            res.status(answer.status).json(answer)
+          }else if (r) {
+            const user = {}
+            user.idUser = r._id
+            user.firstName = r.firstName
+            user.lastName = r.lastName
+            userlist.push(user)
+          }else{
+            answer.status = 200
+            answer.users = userlist
+            res.status(answer.status).json(answer)
+          }
+        })
+      }else{
+        answer.status = 403
+        res.status(answer.status).json(answer)
+      }
+    }
+  )
+}
+
+export { doers }
