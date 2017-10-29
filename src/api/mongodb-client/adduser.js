ce0692095a5ce7b767d92c0299dc568b1b4641cc
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-10-29 10:00:03 +0000

    Token check and little bugs fixed.

--- src/api/mongodb-client/adduser.js
+++ src/api/mongodb-client/adduser.js
@@ -1,61 +1,78 @@
 import { ObjectID } from 'mongodb'
 import crypto from 'crypto'
 
-function addUser  (req, res, db) {
+function addUser (req, res, db) {
   const answer = {}
-  if (
-       !('firstName' in req.body)
-    || !('lastName' in req.body)
-    || !('eMail' in req.body)
-    || !('idManager' in req.body)
-    || !('role' in req.body)
-    || !('password' in req.body)
-  ){
-    answer.status = 400
-    res.status(answer.status).json(answer)
-    return
-  }
 
-  // FIXME validation.
-  // Especially idManager as 24 length and [0-9a-f] symbols
-
-  const pass = crypto.createHash('sha256')
-                 .update(req.body.password)
-                 .digest('hex')
-  const doc = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    manager: {
-      id: req.body.idManager,
-      fullName: ''
-    },
-    eMail: req.body.eMail,
-    role: req.body.role,
-    password: pass
-  }
-  const Users = db.collection('Users')
-  if (req.body.idManager === '0') {
-    doc.manager.fullName = 'None'
-    insertUser(res, Users, doc)
-  }else{
-    doc.manager.id = new ObjectID(doc.manager.id)
-    Users.findOne(
-      {
-        _id: doc.manager.id
-      }, (err, r) => {
-        if (err) {
-          answer.status = 500
+  const Tokens = db.collection('Tokens')
+  Tokens.findOne(
+    {
+      token: req.params.token,
+      role: 'Admin'
+    }, (err, r) => {
+      if (err) {
+        answer.status = 500
+        res.status(answer.status).json(answer)
+      }else if (r) {
+        if (
+             !('firstName' in req.body)
+          || !('lastName' in req.body)
+          || !('eMail' in req.body)
+          || !('idManager' in req.body)
+          || !('role' in req.body)
+          || !('password' in req.body)
+        ){
+          answer.status = 400
           res.status(answer.status).json(answer)
-        }else if (r){
-          doc.manager.fullName = r.firstName+' '+r.lastName
+          return
+        }
+
+        // FIXME validation.
+        // Especially idManager as 24 length and [0-9a-f] symbols
+
+        const pass = crypto.createHash('sha256')
+                       .update(req.body.password)
+                       .digest('hex')
+        const doc = {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          manager: {
+            id: req.body.idManager,
+            fullName: ''
+          },
+          eMail: req.body.eMail,
+          role: req.body.role,
+          password: pass
+        }
+        const Users = db.collection('Users')
+        if (req.body.idManager === '0') {
+          doc.manager.fullName = 'None'
           insertUser(res, Users, doc)
         }else{
-          answer.status = 422
-          res.status(answer.status).json(answer)
+          doc.manager.id = new ObjectID(doc.manager.id)
+          Users.findOne(
+            {
+              _id: doc.manager.id
+            }, (err, r) => {
+              if (err) {
+                answer.status = 500
+                res.status(answer.status).json(answer)
+              }else if (r){
+                doc.manager.fullName = r.firstName+' '+r.lastName
+                insertUser(res, Users, doc)
+              }else{
+                answer.status = 422
+                res.status(answer.status).json(answer)
+              }
+            }
+          )
         }
+      }else{
+        answer.status = 403
+        res.status(answer.status).json(answer)
       }
-    )
-  }
+    }
+  )
 }
 
 function insertUser (res, Users, doc) {
@@ -77,7 +94,6 @@ function insertUser (res, Users, doc) {
           }else{
             answer.status = 200
             answer.idUser = r.insertedId
-            console.log(r)
           }
           res.status(answer.status).json(answer)
         })
