fba03bc83837b48cdaf1aff8949a4613578593de
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-06 18:28:05 +0000

    Render sort field and direction in table sorter.

--- src/spapp/TaskTableHeaderSorter.jsx
+++ src/spapp/TaskTableHeaderSorter.jsx
@@ -3,12 +3,30 @@ import { Button } from './Button.jsx'
 
 class TaskTableHeaderSorter extends Component {
   render() {
+    const captions = {
+      name: 'Name',
+      doer: 'Doer',
+      priority: 'Priority',
+      deadline: 'Deadline'
+    }
+    let direction = ''
+    if (this.props.dir === 'up') {
+      direction = '↑'
+    }else{
+      direction = '↓'
+    }
+    switch (this.props.col) {
+      case 'name': captions.name = direction+captions.name; break
+      case 'doer': captions.doer = direction+captions.doer; break
+      case 'priority': captions.priority = direction+captions.priority; break
+      case 'deadline': captions.deadline = direction+captions.deadline; break
+    }
     return (
       <tr>
-        <th><Button caption="Name" /></th>
-        <th><Button caption="Doer" /></th>
-        <th><Button caption="Priority" /></th>
-        <th><Button caption="Deadline" /></th>
+        <th><Button caption={captions.name} /></th>
+        <th><Button caption={captions.doer} /></th>
+        <th><Button caption={captions.priority} /></th>
+        <th><Button caption={captions.deadline} /></th>
       </tr>
     )
   }
