bd433b4d3bda1935b73f66423dc6f1e9fe5c0425
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-06 19:25:50 +0000

    One more step to fetch from REST API. App state polished.

--- src/spapp/App.jsx
+++ src/spapp/App.jsx
@@ -1,21 +1,43 @@
 import React, { Component } from 'react'
 import { Top } from './Top.jsx'
 import { Desktop } from './Desktop.jsx'
+import './App.less'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       viewer: 'welcome',
-      user: {
-        authorized: true,
-        firstName: 'AlexAnder',
-        lastName: 'Moiseev',
-        eMail: 'alexfisher@mail.ru',
-        isAdmin: true
-      }
+      authorized: false,
+    }
+    this.credentials: {
+      firstName: '',
+      lastName: '',
+      eMail: '',
+      isAdmin: false,
+      token: '',
     }
     this.setViewer = this.setViewer.bind(this)
+    this.didLogin = this.didLogin.bind(this)
+    this.didLogout = this.didLogout.bind(this)
+  }
+
+  didLogin(cred) {
+    this.credentials.firstName = cred.firstName
+    this.credentials.lastName = cred.lastName
+    this.credentials.eMail = cred.eMail
+    this.credentials.isAdmin = cred.isAdmin
+    this.credentials.token = cred.token
+    this.setState( {authirized: true, viewer: 'TaskListViewer'} )
+  }
+
+  didLogout() {
+    this.credentials.firstName = ''
+    this.credentials.lastName = ''
+    this.credentials.eMail = ''
+    this.credentials.isAdmin = false
+    this.credentials.token = ''
+    this.setState( {authirized: false, viewer: 'welcome'} )
   }
 
   setViewer(viewer) {
@@ -24,10 +46,19 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Top {...this.state} setViewer={this.setViewer} />
+      <div className='App-body'><div className='App-ribbon'>
+        <Top
+          authorized={this.state.authorized}
+          isAdmin={this.credentials.isAdmin}
+          firstName={this.credentials.firstName}
+          lastName={this.credentials.lastName}
+          viewer={this.state.viewer}
+          setViewer={this.setViewer}
+          didLogin={this.didLogin}
+          didLogout={this.didLogout}
+        />
         <Desktop viewer={this.state.viewer} setViewer={this.setViewer} />
-      </div>
+      </div></div>
     )
   }
 }
