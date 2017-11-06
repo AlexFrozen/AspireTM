6f1fa920bfb82628ef61b15aa23aee087dabf10c
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-06 18:23:28 +0000

    Set styles to childs and placeholders added.

--- src/spapp/Auth.jsx
+++ src/spapp/Auth.jsx
@@ -3,20 +3,24 @@ import { InputEdit } from './InputEdit.jsx'
 import { InputPass } from './InputPass.jsx'
 import { Label } from './Label.jsx'
 import { Button } from './Button.jsx'
+import './Top.less'
 
 class Auth extends Component {
   render() {
     const elems = []
     if (this.props.authorized) {
-      elems.push(<Label key='1' caption={this.props.firstName + ' ' + this.props.lastName} />)
-      elems.push(<Button key='2' caption='Logout' />)
+      elems.push(<Label key='1' caption={this.props.firstName + ' '
+                                       + this.props.lastName + ' '}/>)
+      elems.push(<Button key='2' className='Menu-button' caption='Logout' />)
     }else{
-      elems.push(<InputEdit key='3' />)
-      elems.push(<InputPass key='4' />)
-      elems.push(<Button key='5' caption='Log in' />)
+      elems.push(<InputEdit key='3' className='Auth-input'
+                            hint='E-Mail address' />)
+      elems.push(<InputPass key='4' className='Auth-input'
+                            hint='Password' />)
+      elems.push(<Button key='5' className='Menu-button' caption='Log in' />)
     }
     return (
-      <div className={this.props.className}>
+      <div className='Auth'>
         {elems}
       </div>
     )
