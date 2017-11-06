85a79e2d26c24b6e44937284b82e01049e9481f5
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-06 18:29:31 +0000

    One more step to sortable table.

--- src/spapp/TaskListViewer.jsx
+++ src/spapp/TaskListViewer.jsx
@@ -4,20 +4,37 @@ import { TaskTable } from './TaskTable.jsx'
 import { TaskTablePager } from './TaskTablePager.jsx'
 
 class TaskListViewer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sortColumn: 'name',
+      sortDirection: 'down',
+      doer: 0,
+      priority: 'all',
+      substr: '',
+      pageSize: 10,
+      page: 1,
+      totalRows: 0
+    }
+  }
   render() {
     const ROWS = [
-      {name: 'Nam1', doer: 'qwer asdf', priority: 'Urgent', deadline: 'weekends'},
-      {name: 'Nam2', doer: 'asdf zxcv', priority: 'Urgent', deadline: 'weekends'},
-      {name: 'Nam3', doer: 'erty sdfg', priority: 'Medium', deadline: 'weekends'},
-      {name: 'Nam4', doer: 'qwer asdf', priority: 'Urgent', deadline: 'weekends'},
-      {name: 'Nam5', doer: 'qwer asdf', priority: 'Medium', deadline: 'weekends'},
+      {name: 'Nam1', doer: 'qwer asdf', priority: 'Urgent', deadline: 'weeks'},
+      {name: 'Nam2', doer: 'asdf zxcv', priority: 'Urgent', deadline: 'weeks'},
+      {name: 'Nam3', doer: 'erty sdfg', priority: 'Medium', deadline: 'weeks'},
+      {name: 'Nam4', doer: 'qwer asdf', priority: 'Urgent', deadline: 'weeks'},
+      {name: 'Nam5', doer: 'qwer asdf', priority: 'Medium', deadline: 'weeks'},
       {name: 'Nam6', doer: 'qwer asdf', priority: 'Urgent', deadline: 'we'},
-      {name: 'Nam7', doer: 'qwer asdf', priority: 'Blocker', deadline: 'tomorrow'},
+      {name: 'Nam7', doer: 'qwer asdf', priority: 'Blocker', deadline: 'tomor'},
     ]
     return (
       <div>
         <TaskTableFilter />
-        <TaskTable rows={ROWS} setViewer={this.props.setViewer} />
+        <TaskTable
+          col={this.state.sortColumn}
+          dir={this.state.sortDirection}
+          rows={ROWS}
+          setViewer={this.props.setViewer} />
         <TaskTablePager pad='4' pages='24' currentPage='9' />
       </div>
     )
