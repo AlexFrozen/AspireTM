e245d31684bcb33d68da7067c40ce1454a70cd86
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-06 18:13:19 +0000

    <input type="button"> changed to <button>

--- src/spapp/Button.jsx
+++ src/spapp/Button.jsx
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 class Button extends Component {
   render() {
     return (
-      <input type="button"
+      <button
         className={this.props.className}
-        value={this.props.caption}
         onClick={this.props.onClick}
-      />
+      >
+        {this.props.caption}
+      </button>
     )
   }
 }
