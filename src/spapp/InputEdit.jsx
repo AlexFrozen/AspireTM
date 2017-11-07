7e4738ac5feb9c5b1b7e39e2cd417a1b1fe29aaf
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-07 05:46:18 +0000

    Default value and onChange props to use.

--- src/spapp/InputEdit.jsx
+++ src/spapp/InputEdit.jsx
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 class InputEdit extends Component {
   render() {
     return (
-      <input className={this.props.className} type='text'
-             placeholder={this.props.hint} />
+      <input
+        className={this.props.className}
+        type='text'
+        placeholder={this.props.hint}
+        value={this.props.value}
+        onChange={this.props.onChange}
+      />
     )
   }
 }
--- src/spapp/InputPass.jsx
+++ src/spapp/InputPass.jsx
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 class InputPass extends Component {
   render() {
     return (
-      <input className={this.props.className} type='password'
-             placeholder={this.props.hint} />
+      <input
+        className={this.props.className}
+        type='password'
+        placeholder={this.props.hint}
+        value={this.props.value}
+        onChange={this.props.onChange}
+      />
     )
   }
 }
