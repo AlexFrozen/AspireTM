d3f254845083b4e873138e17a44a8cdf5f452610
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-09 20:22:44 +0000

    Oh ESLint! First randevue! :)

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+/* eslint no-console: "off" */
 import { MongoClient } from 'mongodb'
 import assert from 'assert'
 import crypto from 'crypto'
@@ -6,34 +7,42 @@ const login = 'admin@no.mail'
 const pwd = 'admin'
 const pass = crypto.createHash('sha256').update(pwd).digest('hex')
 
-MongoClient.connect('mongodb://localhost:27017/aspire-tm', (err, db) => {
-  assert.equal(null, err);
-  console.log('Connected successfully to MongoDB server');
+MongoClient.connect(
+  'mongodb://localhost:27017/aspire-tm',
+  (errorConnection, db) => {
+    assert.equal(null, errorConnection)
+    console.log('Connected successfully to MongoDB server')
 
-  const col = db.collection('Users')
-  col.findOneAndUpdate({eMail:'admin@no.mail'}, {$set: {password:pass}}, (err, r) => {
-    if (r.value == null) {
-      console.log('Default user is not found, trying to create...')
-      col.insertOne({
-        firstName: 'Aspire',
-        lastName: 'Administrator',
-        manager: {
-          id: 0,
-          fullName: 'None'
-        },
-        eMail: login,
-        role: 'Admin',
-        password: pass
-      }, (err, r) => {
-        if (r.insertedCount != 1) {
-          console.log("Couldn't create default user. Sorry...")
+    const col = db.collection('Users')
+    col.findOneAndUpdate(
+      {eMail:'admin@no.mail'},
+      {$set: {password:pass}},
+      (errorFindAdmin, resAdminFound) => {
+        if (resAdminFound.value == null) {
+          console.log('Default user is not found, trying to create...')
+          col.insertOne({
+            firstName: 'Aspire',
+            lastName: 'Administrator',
+            manager: {
+              id: 0,
+              fullName: 'None',
+            },
+            eMail: login,
+            role: 'Admin',
+            password: pass,
+          }, (errorCreateAdmin, createdAdmin) => {
+            if (createdAdmin.insertedCount !== 1) {
+              console.log("Couldn't create default user. Sorry...")
+            }else{
+              console.log(`Created default user ${login} with pass ${pwd}.`)
+              console.log("Don't forget to change!")
+            }
+          })
         }else{
-          console.log('Created default user ' + login + ' with pass ' + pwd + '. Don\'t forget to change!')
+          console.log(`Password for default user ${login} reset to ${pwd}`)
         }
-      })
-    }else{
-      console.log('Password for default user ' + login + ' reset to ' + pwd)
-    }
-    db.close()
-  })
-})
+        db.close()
+      }
+    )
+  }
+)
