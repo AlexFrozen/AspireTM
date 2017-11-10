c7cee5c6607877664d7adba639a2377056fa9552
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-10 01:31:37 +0000

    Linting, linting...

--- src/api/mongodb-client/doers.js
+++ src/api/mongodb-client/doers.js
@@ -2,35 +2,32 @@ function doers (req, res, db) {
   const answer = {}
 
   const Tokens = db.collection('Tokens')
-  Tokens.findOne({
-    token: req.params.token,
-  }, (err, r) => {
-    if (err) {
+  Tokens.findOne({ token: req.params.token }, (errorFindToken, resToken) => {
+    if (errorFindToken) {
       answer.status = 500
       res.status(answer.status).json(answer)
-    } else if (r) {
-      const idManager = r.idUser
+    } else if (resToken) {
+      const idManager = resToken.idUser
       const Users = db.collection('Users')
       const userlist = []
 
-      Users.find({
-        'manager.id': idManager,
-      }).each((err, r) => {
-        if (err) {
-          answer.status = 500
-          res.status(answer.status).json(answer)
-        } else if (r) {
-          const user = {}
-          user.idUser = r._id
-          user.firstName = r.firstName
-          user.lastName = r.lastName
-          userlist.push(user)
-        } else {
-          answer.status = 200
-          answer.users = userlist
-          res.status(answer.status).json(answer)
-        }
-      })
+      Users.find({ 'manager.id': idManager })
+        .each((errorListDoers, resDoer) => {
+          if (errorListDoers) {
+            answer.status = 500
+            res.status(answer.status).json(answer)
+          } else if (resDoer) {
+            const user = {}
+            user.idUser = resDoer._id
+            user.firstName = resDoer.firstName
+            user.lastName = resDoer.lastName
+            userlist.push(user)
+          } else {
+            answer.status = 200
+            answer.users = userlist
+            res.status(answer.status).json(answer)
+          }
+        })
     } else {
       answer.status = 403
       res.status(answer.status).json(answer)
