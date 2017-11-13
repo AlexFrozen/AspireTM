2c231fbf8357b844f426f90e49422ebbd3ab60aa
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-13 02:54:56 +0000

    propTypes check added.

--- src/spapp/UsersTable.jsx
+++ src/spapp/UsersTable.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { UsersTableHeaderSorter } from './UsersTableHeaderSorter.jsx'
 import { TableRow } from './TableRow.jsx'
 import './UsersTable.less'
 
 class UsersTable extends Component {
+  static propTypes = {
+    rows: PropTypes.arrayOf(
+      PropTypes.shape({
+        firstName: PropTypes.string,
+        lastName: PropTypes.string,
+        eMail: PropTypes.string,
+        manager: PropTypes.string,
+        role: PropTypes.oneOf(['Admin', 'User']),
+      })
+    ).isRequired,
+  }
+
   render() {
     const rows = []
     let bkey = 1
     this.props.rows.forEach((row) => {
       rows.push(<TableRow
         key={bkey++}
-        className='UsersTable-row'
+        looktype='UsersTable'
         cols={
           [row.firstname, row.lastname, row.email, row.manager, row.role]
         }
