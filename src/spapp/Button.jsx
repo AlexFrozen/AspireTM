def2b2f9fb628402faff1aa5930f3c3d0655f9c7
AlexAnder Moiseev <alexander_git@neuromasters.net>
2017-11-13 02:54:35 +0000

    prop-types check.

--- src/spapp/Button.jsx
+++ src/spapp/Button.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Button extends Component {
+  static propTypes = {
+    caption: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    className: PropTypes.string.isRequired,
+  }
+
   render() {
     return (
       <button
