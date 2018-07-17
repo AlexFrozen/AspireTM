728dae7801b1975cddffcec9cba60f335db5e91d
AlexAnder Moiseev <alexander_git@neuromasters.net>
2018-07-17 11:49:54 +0000

    MaterialUI/Grid integrated, props changed

--- src/spapp/SetOfButtons.jsx
+++ src/spapp/SetOfButtons.jsx
@@ -1,27 +1,67 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
 import PropTypes from 'prop-types'
 import { Button } from './Button.jsx'
 
 class SetOfButtons extends Component {
   static propTypes = {
-    buttons: PropTypes.array.isRequired,
-    classKind: PropTypes.string.isRequired,
+    buttons: PropTypes.arrayOf(
+      PropTypes.shape({
+        variant: PropTypes.oneOf([
+          'menu',
+          'simple',
+        ]),
+        caption: PropTypes.string.isRequired,
+        onClick: PropTypes.func.isRequired,
+      }).isRequired
+    ).isRequired,
+    variant: PropTypes.oneOf([
+      'vertical',
+      'horizontal',
+    ]),
   }
+  static defaultProps = { variant: 'horizontal' }
+
   render() {
+    let mui_alignItems, mui_justify, mui_direction
+    switch (this.props.variant) {
+      case 'vertical':
+        mui_alignItems = 'center'
+        mui_justify = 'center'
+        mui_direction = 'column'
+        break
+      case 'horizontal':
+        mui_alignItems = 'center'
+        mui_justify = 'center'
+        mui_direction = 'row'
+        break
+    }
     const buttons = []
     let bkey = 1
     this.props.buttons.forEach((button) => {
-      buttons.push(<Button
-        key={bkey++}
-        classKind={button.classKind}
-        caption={button.caption}
-        onClick={button.onClick}
-      />)
+      buttons.push(
+        <Grid
+          item
+          key={bkey++}
+        >
+          <Button
+            key={bkey++}
+            variant={button.variant}
+            caption={button.caption}
+            onClick={button.onClick}
+          />
+        </Grid>
+      )
     })
     return (
-      <div className={this.props.classKind}>
+      <Grid
+        container
+        alignItems={mui_alignItems}
+        justify={mui_justify}
+        direction={mui_direction}
+      >
         {buttons}
-      </div>
+      </Grid>
     )
   }
 }
